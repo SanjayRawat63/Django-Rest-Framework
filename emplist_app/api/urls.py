fcbce0edebd7dbed6943552b99a634f757b24ee7
Sanjay Rawat <63999697+SanjayRawat63@users.noreply.github.com>
2022-07-14 16:33:12 +0530

    usingAPIViewClass

--- emplist_app/api/urls.py
+++ emplist_app/api/urls.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path
-from emplist_app.api.views import employee_details, employees_list
+from emplist_app.api.views import EmployeeDetails,EmployeesList
 urlpatterns = [
-    path('emplist/', employees_list, name="emp-list"),
-    path('<int:pk>/', employee_details, name="emp-details")
+    path('emplist/', EmployeesList.as_view(), name="emp-list"),
+    path('<int:pk>/', EmployeeDetails.as_view(), name="emp-details")
 ]
--- emplist_app/api/views.py
+++ emplist_app/api/views.py
@@ -4,35 +4,34 @@ import re
 from emplist_app.api.serializers import EmployeeSerializer
 from emplist_app.models import employees
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import APIView
 from rest_framework import status
-@api_view(['GET','POST'])
-def employees_list(request):
-    if request.method == 'GET':
-        emp_list=employees.objects.all()
-        serializer=EmployeeSerializer(emp_list,many=True)
-        return Response(serializer.data)
-    if request.method == 'POST':
-        serializer=EmployeeSerializer(data=request.data)
+class EmployeesList(APIView):
+    
+    def get(self,request):
+         emp_list = employees.objects.all()
+         serializer=EmployeeSerializer(emp_list,many=True)
+         return Response(serializer.data)
+     
+    def put(self,request):
+        serializer = EmployeeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
- 
- 
-@api_view(['GET','PUT','DELETE'])
-def employee_details(request,pk):
+         
+class EmployeeDetails(APIView):
     
-    if request.method == 'GET':
+    def get(self,request,pk):
         try:
-            emp_list=employees.objects.get(pk=pk)
+            emp_list = employees.objects.get(pk=pk)
         except employees.DoesNotExist:
              return Response({"Error":"Employee Details Not Found"},status=status.HTTP_404_NOT_FOUND)
         serializer=EmployeeSerializer(emp_list)
         return Response(serializer.data)
     
-    if request.method=='PUT':
+    def put(self,request,pk):
         try:
             emp_list = employees.objects.get(pk=pk)
         except employees.DoesNotExist:
@@ -44,9 +43,8 @@ def employee_details(request,pk):
         else:
             return Response(serializer.errors)
         
-    
-    if request.method=='DELETE':
+    def delete(self,request,pk):
         emp_list=employees.objects.get(pk=pk)
         emp_list.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-        
+ 
